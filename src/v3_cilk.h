a40753782765c9f0ca8e65ecc227863ee24ea545
Stavros Filosidis <stavrosfil@gmail.com>
2020-11-24 23:42:54 +0200

    Use cilk

--- src/v3_cilk.h
+++ src/v3_cilk.h
@@ -5,22 +5,22 @@
 #include <cilk/cilk_api.h>
 
 uint32_t hasEdge(uint32_t a, uint32_t b, uint32_t *csr_row, uint32_t *csr_col) {
-    for (int i = csr_row[b]; i < csr_row[b + 1]; i++)
-        if (a == csr_col[i])
+    for (int i = csr_row[a]; i < csr_row[a + 1]; i++)
+        if (b == csr_col[i])
             return 1;
     return 0;
 }
 
 uint32_t triangleCountV3Cilk(int N, uint32_t *csr_row, uint32_t *csr_col, uint32_t *c3) {
-    // cilk::reducer_opadd<uint32_t> triangles;
-    uint32_t triangles = 0;
 
-    // int gs         = 10000;
-    // int numWorkers = __cilkrts_get_nworkers();
-    // printf("Workers: %d\n", numWorkers);
+    cilk::reducer_opadd<uint32_t> triangles;
+
+    int gs         = 10000;
+    int numWorkers = __cilkrts_get_nworkers();
+    printf("Workers: %d\n", numWorkers);
 
     // #pragma grainsize = gs
-    for (int i = 0; i < N - 2; i++) {
+    cilk_for(int i = 0; i < N - 2; i++) {
 
         int colStartPtr = csr_row[i];
         int colEndPrt   = csr_row[i + 1];
@@ -30,18 +30,14 @@ uint32_t triangleCountV3Cilk(int N, uint32_t *csr_row, uint32_t *csr_col, uint32
             if (a > i)
                 for (int k = j + 1; k < colEndPrt; k++) {
                     int b = csr_col[k];
-                    // printf("%d %d\n", a, b);
-                    // if (hasEdge(a, b, csr_row, csr_col) || hasEdge(b, a, csr_row, csr_col)) {
-                    //     triangles++;
-                    //     c3[i]++;
-                    //     c3[a]++;
-                    //     c3[b]++;
-                    // }
-                    // triangles += hasEdge(a, b, csr_row, csr_col);
-                    triangles += hasEdge(b, a, csr_row, csr_col);
+                    if (hasEdge(a, b, csr_row, csr_col)) {
+                        triangles++;
+                        c3[i]++;
+                        c3[a]++;
+                        c3[b]++;
+                    }
                 }
         }
     }
-    // return triangles.get_value();
-    return triangles;
+    return triangles.get_value();
 }
