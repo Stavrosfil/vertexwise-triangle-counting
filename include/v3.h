f5e5ae18cf0bbd36a34477a5b8d0827614869cf0
Stavros Filosidis <stavrosfil@gmail.com>
2020-12-04 03:58:34 +0200

    Refined v3 for whole matrix usage

--- include/v3.h
+++ include/v3.h
@@ -1,35 +1,39 @@
 #include <stdint.h>
 #include <stdio.h>
 
-// uint32_t hasEdge(uint32_t a, uint32_t b, uint32_t *csr_row, uint32_t *csr_col) {
-//     for (int i = csr_row[a]; i < csr_row[a + 1]; i++)
-//         if (b == csr_col[i])
-//             return 1;
-//     return 0;
-// }
+bool hasEdge(uint32_t a, uint32_t b, uint32_t csr_row_ptr[], uint32_t csr_col[]) {
 
-uint32_t triangleCountV3(int N, uint32_t *csr_row, uint32_t *csr_col, uint32_t *c3) {
+    uint32_t lo  = csr_row_ptr[a];
+    uint32_t hi  = csr_row_ptr[a + 1] - 1;
+    uint32_t mid = (lo + hi) / 2;
 
-    uint32_t triangles = 0;
+    while (lo <= hi) {
+        if (csr_col[mid] == b)
+            return true;
+        else if (csr_col[mid] < b)
+            lo = mid + 1;
+        else
+            hi = mid - 1;
+        mid = (lo + hi) / 2;
+    }
+    return false;
+}
+
+void triangleCountV3(int N, uint32_t *c3, uint32_t *csr_row_ptr, uint32_t *csr_col) {
 
     for (int i = 0; i < N - 2; i++) {
 
-        int colStartPtr = csr_row[i];
-        int colEndPrt   = csr_row[i + 1];
+        int colStartPtr = csr_row_ptr[i];
+        int colEndPrt   = csr_row_ptr[i + 1];
 
         for (int j = colStartPtr; j < colEndPrt; j++) {
             int a = csr_col[j];
-            if (a > i)
-                for (int k = j + 1; k < colEndPrt; k++) {
-                    int b = csr_col[k];
-                    if (hasEdge(a, b, csr_row, csr_col)) {
-                        triangles++;
-                        c3[i]++;
-                        c3[a]++;
-                        c3[b]++;
-                    }
+            for (int k = j + 1; k < colEndPrt; k++) {
+                int b = csr_col[k];
+                if (hasEdge(a, b, csr_row_ptr, csr_col)) {
+                    c3[i]++;
                 }
+            }
         }
     }
-    return triangles;
 }
