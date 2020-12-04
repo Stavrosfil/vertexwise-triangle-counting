1415b09543a0fc69e530184ef650a68622ca93c8
Stavros Filosidis <stavrosfil@gmail.com>
2020-12-04 04:03:40 +0200

    Use binary search for edge searching

--- include/v3.h
+++ include/v3.h
@@ -1,23 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
-
-bool hasEdge(uint32_t a, uint32_t b, uint32_t csr_row_ptr[], uint32_t csr_col[]) {
-
-    uint32_t lo  = csr_row_ptr[a];
-    uint32_t hi  = csr_row_ptr[a + 1] - 1;
-    uint32_t mid = (lo + hi) / 2;
-
-    while (lo <= hi) {
-        if (csr_col[mid] == b)
-            return true;
-        else if (csr_col[mid] < b)
-            lo = mid + 1;
-        else
-            hi = mid - 1;
-        mid = (lo + hi) / 2;
-    }
-    return false;
-}
+#include "../include/matrixFunctions.h"
 
 void triangleCountV3(int N, uint32_t *c3, uint32_t *csr_row_ptr, uint32_t *csr_col) {
 
--- include/v3_openmp.h
+++ include/v3_openmp.h
@@ -2,46 +2,24 @@
 #include <stdio.h>
 #include <omp.h>
 #include <math.h>
+#include "../include/matrixFunctions.h"
 
-int hasEdge(uint32_t a, uint32_t b, uint32_t *csr_row, uint32_t *csr_col) {
-    for (int i = csr_row[a]; i < csr_row[a + 1]; i++)
-        if (b == csr_col[i])
-            return 1;
-    return 0;
-}
+void triangleCountV3(int N, uint32_t *c3, uint32_t *csr_row_ptr, uint32_t *csr_col) {
 
-uint32_t partitioner(int N, int chunks, int currentChunk) {
-    int y = sqrt(currentChunk * currentChunk + N * N / 8);
-    return y;
-}
-
-uint32_t triangleCountV3OpenMP(int N, uint32_t *csr_row, uint32_t *csr_col, uint32_t *c3) {
-
-    uint32_t triangles = 0;
-    int chunks         = 8;
-
-#pragma omp parallel for schedule(dynamic) reduction(+ : triangles)
+#pragma omp parallel for schedule(dynamic)
     for (int i = 0; i < N - 2; i++) {
-
-        int tempTriangles = 0;
-
-        int colStartPtr = csr_row[i];
-        int colEndPrt   = csr_row[i + 1];
+        int colStartPtr = csr_row_ptr[i];
+        int colEndPrt   = csr_row_ptr[i + 1];
 
         for (int j = colStartPtr; j < colEndPrt; j++) {
             int a = csr_col[j];
-            if (a > i)
-                for (int k = j + 1; k < colEndPrt; k++) {
-                    int b = csr_col[k];
-                    if (hasEdge(a, b, csr_row, csr_col)) {
-                        tempTriangles++;
-                        // c3[i]++;
-                        // c3[a]++;
-                        // c3[b]++;
-                    }
+            for (int k = j + 1; k < colEndPrt; k++) {
+                int b = csr_col[k];
+                if (hasEdge(a, b, csr_row_ptr, csr_col)) {
+#pragma omp critical
+                    c3[i]++;
                 }
+            }
         }
-        triangles += tempTriangles;
     }
-    return triangles;
 }
