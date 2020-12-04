1e3ef809425706f789a2b07b62e512b9dbc2cf33
Stavros Filosidis <stavrosfil@gmail.com>
2020-12-04 02:29:14 +0200

    Pthreads dynamic scheduling done

--- src/v4_pthreads.h
+++ src/v4_pthreads.h
@@ -2,10 +2,13 @@
 #include <stdio.h>
 #include "pthread.h"
 
-#define NUM_THREADS 10000
+#define NUM_THREADS 8
+
+pthread_mutex_t lock;
+uint32_t iteration = 0;
 
 struct data {
-    uint32_t lo, hi, *c, *csr_row_ptr, *csr_col;
+    uint32_t N, *c, *csr_row_ptr, *csr_col;
 };
 
 void multiplyRowCol(uint32_t row, uint32_t *c, uint32_t col, uint32_t *csr_row_ptr, uint32_t *csr_col) {
@@ -20,57 +23,44 @@ void multiplyRowCol(uint32_t row, uint32_t *c, uint32_t col, uint32_t *csr_row_p
             c[row]++;
             i++;
             j++;
-        } else if (csr_col[i] < csr_col[j]) {
+        } else if (csr_col[i] < csr_col[j])
             i++;
-        } else {
+        else
             j++;
-        }
     }
-    // return ans;
 }
 
 void *multiplyMatrixVector(void *args) {
 
     struct data *d = (struct data *)args;
+    uint32_t i     = 0;
 
-    // uint32_t ans = 0;
+    while (iteration < d->N) {
 
-    for (int i = d->lo; i < d->hi; i++) {
-        for (int j = d->csr_row_ptr[i]; j < d->csr_row_ptr[i + 1]; j++) {
+        pthread_mutex_lock(&lock);
+        i = iteration++;
+        pthread_mutex_unlock(&lock);
+
+        for (int j = d->csr_row_ptr[i]; j < d->csr_row_ptr[i + 1]; j++)
             multiplyRowCol(i, d->c, d->csr_col[j], d->csr_row_ptr, d->csr_col);
-        }
+        d->c[i] /= 2;
     }
-
-    // pthread_exit(NULL);
-    return NULL;
+    pthread_exit(NULL);
 }
 
 void triangleCountV4(uint32_t N, uint32_t *c, uint32_t *csr_row_ptr, uint32_t *csr_col) {
 
     pthread_t threads[NUM_THREADS];
+    struct data thread_data;
 
-    struct data thread_data[NUM_THREADS];
+    thread_data.N           = N;
+    thread_data.c           = c;
+    thread_data.csr_row_ptr = csr_row_ptr;
+    thread_data.csr_col     = csr_col;
 
-    uint32_t chunk_size = N / NUM_THREADS;
-
-    for (int i = 0; i < NUM_THREADS; i++) {
-        thread_data[i].c           = c;
-        thread_data[i].lo          = i * chunk_size;
-        thread_data[i].hi          = (i + 1) * chunk_size;
-        thread_data[i].csr_row_ptr = csr_row_ptr;
-        thread_data[i].csr_col     = csr_col;
-    }
-    thread_data[NUM_THREADS - 1].hi = N;
-
-    // Starting the threads
     for (int i = 0; i < NUM_THREADS; i++)
-        pthread_create(&threads[i], NULL, multiplyMatrixVector, &thread_data[i]);
+        pthread_create(&threads[i], NULL, multiplyMatrixVector, &thread_data);
 
-    // And waiting for the threads to finish
     for (int i = 0; i < NUM_THREADS; i++)
         pthread_join(threads[i], NULL);
-
-    // for (int i = 0; i < N; i++) {
-    // multiplyMatrixVector(i, c, csr_row_ptr, csr_col);
-    // }
 }
